914b6957eb70f0497c7c68562e4addec0a3683dd
Enes <54481799+eneskuluk@users.noreply.github.com>
2020-02-25 03:57:14 +0300

    sign-in+up and transition to realtime from firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,199 @@
+// import React from 'react';
+// import './App.css';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/database';
+// import 'firebase/auth';
+//
+//
+//
+// const config = {
+//   apiKey: "AIzaSyBdkoTn81MIQMGLHgExwJ5jS1KLWyn6As8",
+//   authDomain: "ineedcapstone.firebaseapp.com",
+//   databaseURL: "https://ineedcapstone.firebaseio.com",
+//   projectId: "ineedcapstone",
+//   storageBucket: "ineedcapstone.appspot.com",
+//   messagingSenderId: "171791021581",
+//   appId: "1:171791021581:web:01085a0bcdee9a79f9c744",
+//   measurementId: "G-STQ0VNRVBQ"
+// };
+// firebase.initializeApp( config );
+// //let db = firebase.firestore();
+// let db=firebase.database();
+// /*var citiesRef = db.collection('cities');
+//
+//  var landmarks = Promise.all([
+//  citiesRef.doc('SF').collection('needs').doc().set({
+//  location: 'sakarya',
+//  type: 'book',
+//  status:true,
+//  desc:"need help with missing books in karasu's library"
+//
+//  })
+//  ]);*/
+//
+//
+// //var needs = db.collectionGroup('needs').where('status', '==', false);
+// /*needs.get().then(function (querySnapshot) {
+//  querySnapshot.forEach(function (doc) {
+//  console.log(doc.id, ' => ', doc.data());
+//  });
+//  });*/
+//
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       cities:['Select a City','Adana', 'Adıyaman', 'Afyon', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin',
+//         'Aydın', 'Balıkesir', 'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale',
+//         'Çankırı', 'Çorum', 'Denizli', 'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir',
+//         'Gaziantep', 'Giresun', 'Gümüşhane', 'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'istanbul', 'izmir',
+//         'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli', 'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya',
+//         'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş', 'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya',
+//         'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat', 'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak',
+//         'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman', 'Kırıkkale', 'Batman', 'Şırnak',
+//         'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye', 'Düzce'],
+//       selectedcity:"",
+//       type:['Type Of Need','Clothing','Electronics','Books'],
+//       keywords:"",
+//       selectedtype:"",
+//       getsearch:[],
+//       searched:false,
+//     };
+//
+//     //this.insertNeed=this.insertNeed.bind(this);
+//     this.insertNeedrealtime=this.insertNeedrealtime.bind(this);
+//   }
+//
+//
+//   /*insertNeed(event){
+//     event.preventDefault();
+//     var citiesRef = db.collection('cities');
+//
+//     var landmarks = Promise.all([
+//       citiesRef.doc(this.state.selectedcity.toLowerCase()).collection('needs').doc().set({
+//         location: this.state.selectedcity,
+//         type: this.state.selectedtype,
+//         status:false,
+//         desc:this.state.keywords,
+//
+//       })
+//     ]);
+//     console.log("itworks?");
+//     console.log(this.state.selectedcity);
+//     console.log(this.state.selectedtype);
+//     console.log(this.state.keywords);
+//     this.setState({getsearch:[],searched:false});
+//   }*/
+//   insertNeedrealtime(event){
+//     event.preventDefault();
+//     //var citiesRef = db.collection('cities');
+//
+//     db.ref('cities/'+ this.state.selectedcity.toLowerCase()+'/needs/').push({
+//       location: this.state.selectedcity,
+//       type: this.state.selectedtype,
+//       status:false,
+//       desc:this.state.keywords,
+//     });
+//
+//     console.log("itworks?");
+//     console.log(this.state.selectedcity);
+//     console.log(this.state.selectedtype);
+//     console.log(this.state.keywords);
+//     this.setState({getsearch:[],searched:false});
+//
+//   }
+//   componentDidMount() {
+//     this.interval = setInterval(() => this.setState({ time: Date.now() }), 500);
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
+//   searchNeedrealtime=(event)=>{
+//     event.preventDefault();
+//     if (this.state.selectedcity==="izmir"||this.state.selectedcity==="istanbul"||this.state.selectedcity==="Sakarya"||this.state.selectedcity==="Adana"||this.state.selectedcity==="Ankara"){
+//       let i = 0;
+//       event.preventDefault();
+//       let { getsearch } = this.state;
+//       //et needs = db.collectionGroup( 'needs' ).where( 'location', '==', this.state.selectedcity.toLowerCase() );
+//       let needs=db.ref('cities/'+this.state.selectedcity.toLowerCase()+'/needs/');
+//       needs.
+//       needs.get().then( function ( querySnapshot ){
+//         querySnapshot.forEach( function ( doc ){
+//           //console.log( doc.id, ' => ', doc.data() );
+//           getsearch[ i ] = doc.data();
+//           i++;
+//         } );
+//       } );
+//       this.setState( { getsearch,searched:true } );
+//
+//     }
+//     else{
+//       this.setState({getsearch:[],searched:false});
+//     }
+//   }
+//
+//   // searchNeed=(event)=>{
+//   //   event.preventDefault();
+//   //   if (this.state.selectedcity==="izmir"||this.state.selectedcity==="istanbul"||this.state.selectedcity==="Sakarya"||this.state.selectedcity==="Adana"||this.state.selectedcity==="Ankara"){
+//   //     let i = 0;
+//   //     event.preventDefault();
+//   //     let { getsearch } = this.state;
+//   //     let needs = db.collectionGroup( 'needs' ).where( 'location', '==', this.state.selectedcity.toLowerCase() );
+//   //     needs.get().then( function ( querySnapshot ){
+//   //       querySnapshot.forEach( function ( doc ){
+//   //         //console.log( doc.id, ' => ', doc.data() );
+//   //         getsearch[ i ] = doc.data();
+//   //         i++;
+//   //       } );
+//   //     } );
+//   //     this.setState( { getsearch,searched:true } );
+//   //
+//   //   }
+//   //   else{
+//   //     this.setState({getsearch:[],searched:false});
+//   //   }
+//   // }
+//
+//   render(){
+//     const {getsearch}=this.state;
+//     return (
+//         <div className id="demo">
+//           <div className="sidenav">
+//             <input type="text" name="keywords" value={this.state.keywords} onChange={(e) => this.setState({keywords: e.target.value,getsearch})} placeholder="Enter Keywords or Description"/>
+//             <form onSubmit={this.searchNeed}>
+//               <select value={this.state.selectedtype}
+//                       onChange={(e) => this.setState({selectedtype: e.target.value,getsearch})}>
+//                 {this.state.type.map((x)=><option key={x}>{x}</option>)}
+//               </select>
+//               <select value={this.state.selectedcity}
+//                       onChange={(e) => this.setState({selectedcity: e.target.value,getsearch})}>
+//                 {this.state.cities.map((x)=><option key={x}>{x}</option>)}
+//               </select>
+//               <input type="submit" value="Search" />
+//             </form>
+//             {getsearch.map(((value, index) => <p><strong>{index+1}</strong><br></br><strong>Description:</strong>{value.desc}<br></br><strong>Location:</strong>{value.location}<br></br><strong>Type:</strong>{value.type}</p>))}
+//             <form onSubmit={this.insertNeedrealtime}>
+//               <input type="submit" value="Add Need" />
+//             </form>
+//           </div>
+//         </div>
+//     );
+//   }
+// }
+//
+// export default App;
+
+
+
+
+
 import React from 'react';
 import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/database';
+import "firebase/auth";
 
 
 
@@ -15,9 +207,9 @@ const config = {
   appId: "1:171791021581:web:01085a0bcdee9a79f9c744",
   measurementId: "G-STQ0VNRVBQ"
 };
-firebase.initializeApp( config );
+firebase.initializeApp(config);
 let db = firebase.firestore();
-
+let rdb=firebase.database();
 /*var citiesRef = db.collection('cities');
 
  var landmarks = Promise.all([
@@ -60,11 +252,57 @@ class App extends React.Component{
     };
 
     this.insertNeed=this.insertNeed.bind(this);
+    this.insertNeedrealtime=this.insertNeedrealtime.bind(this);
+    this.signin=this.signin.bind();
+    this.signup=this.signup.bind();
+
+  }
+  insertNeedrealtime(event){
+    event.preventDefault();
+    //var citiesRef = db.collection('cities');
+
+    rdb.ref('cities/'+ this.state.selectedcity.toLowerCase()+'/needs/').push({
+      location: this.state.selectedcity,
+      type: this.state.selectedtype,
+      status:false,
+      desc:this.state.keywords,
+    });
+
+    console.log("itworks?");
+    console.log(this.state.selectedcity);
+    console.log(this.state.selectedtype);
+    console.log(this.state.keywords);
+    this.setState({getsearch:[],searched:false});
+
+  }
+  async signup(){
+    //event.preventDefault();
+    let email="faqeacc@yandex.com";
+    try{
+      console.log(await firebase.auth().createUserWithEmailAndPassword( email, "333221" ));
+      // eslint-disable-next-line no-restricted-globals
+      history.push( "/" );
+    }catch ( error ){
+      alert(error);
+    }
+
+  }
+  async signin(){
+    //event.preventDefault();
+    let email="faqeacc@yandex.com";
+    try{
+      console.log(await firebase.auth().signInWithEmailAndPassword( email, "333221" ));
+      // eslint-disable-next-line no-restricted-globals
+      history.push( "/" );
+    }catch ( error ){
+      alert(error);
+    }
+
   }
 
 
-  insertNeed(event){
-    event.preventDefault();
+  insertNeed(){
+    //event.preventDefault();
     var citiesRef = db.collection('cities');
 
     var landmarks = Promise.all([
@@ -81,7 +319,32 @@ class App extends React.Component{
     console.log(this.state.selectedtype);
     console.log(this.state.keywords);
     this.setState({getsearch:[],searched:false});
+    this.SignIn();
   }
+  searchNeedrealtime=(event)=>{
+    event.preventDefault();
+    if (this.state.selectedcity==="izmir"||this.state.selectedcity==="istanbul"||this.state.selectedcity==="Sakarya"||this.state.selectedcity==="Adana"||this.state.selectedcity==="Ankara"){
+      let i = 0;
+      event.preventDefault();
+      let { getsearch } = this.state;
+      //et needs = db.collectionGroup( 'needs' ).where( 'location', '==', this.state.selectedcity.toLowerCase() );
+      let needs=db.ref('cities/'+this.state.selectedcity.toLowerCase()+'/needs/');
+      //needs.
+      needs.get().then( function ( querySnapshot ){
+        querySnapshot.forEach( function ( doc ){
+          //console.log( doc.id, ' => ', doc.data() );
+          getsearch[ i ] = doc.data();
+          i++;
+        } );
+      } );
+      this.setState( { getsearch,searched:true } );
+
+    }
+    else{
+      this.setState({getsearch:[],searched:false});
+    }
+  };
+
   componentDidMount() {
     this.interval = setInterval(() => this.setState({ time: Date.now() }), 500);
   }
@@ -109,7 +372,8 @@ class App extends React.Component{
     else{
       this.setState({getsearch:[],searched:false});
     }
-  }
+    this.SignUp();
+  };
 
   render(){
     const {getsearch}=this.state;
@@ -140,3 +404,4 @@ class App extends React.Component{
 }
 
 export default App;
+
