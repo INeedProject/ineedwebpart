d25515e9944bb788ddd55405de41b5001628e2ff
Rigormortis <myyilmaz97@gmail.com>
2020-05-05 14:31:28 +0300

    NeedMap with NeedPanel

--- src/Components/NeedMap.js
+++ src/Components/NeedMap.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import NeedPanel from '../Components/NeedPanel';
+
 const mapStyles = {
-  width: '95%',
-  height: '85%',
+  width: '100%',
+  height: '100%',
 }
 
 class NeedMap extends React.Component{
   constructor(props){
     super(props);
     this.state={
-    markers:this.displayMarkers(),
+    markers:this.renderMarkers(),
     loading:true,
+    currentMarker: null,
     }
   }
 componentDidMount(){
     this.forceUpdate();
 }
 
-  displayMarkers = () => {
+  onMarkerClick = (val) => {
+    this.setState({currentMarker: val});
+  };
+
+  renderMarkers = () => {
     console.log(this.props.fetch);
     const val=this.props.fetch.map((val, index) =>
        <Marker key={index} position={{
         lat: val.latitude,
         lng: val.longitude
-      }} onClick={() => alert("You clicked to a marker! This will be need information with Modal style.")} />
+      }} onClick={() => this.onMarkerClick(val)} />
     )
     return val;
 
@@ -34,19 +41,23 @@ componentDidMount(){
   }
 
   render(){
-    return (<LoadScript
-            googleMapsApiKey={process.env.REACT_APP_API_KEY}
-        >
+    return (
+    <>
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_API_KEY}
+      >
         <GoogleMap
-            mapContainerStyle={mapStyles}
-            zoom={10}
-            center={{lat:39.873881,lng:32.748357}}
-            onLoad={this.loaded}
+          mapContainerStyle={mapStyles}
+          zoom={10}
+          center={{lat:39.873881,lng:32.748357}}
+          onLoad={this.loaded}
         >
           {this.state.loading===true?<></>:this.state.markers}
         </GoogleMap>
-        </LoadScript>
-    );
+      </LoadScript>
+      {!!this.state.currentMarker && <NeedPanel value={this.state.currentMarker}/>}
+    </>
+  );
   }
 
 }
