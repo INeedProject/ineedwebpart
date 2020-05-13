de4d73959d74943bd4ba433e61a26078171d4d46
Rigormortis <myyilmaz97@gmail.com>
2020-05-13 15:48:39 +0300

    NeedPanel buttons and styles

--- src/Components/NeedPanel/index.js
+++ src/Components/NeedPanel/index.js
@@ -1,29 +1,43 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import './style.scss'
 
-export default class NeedPanel extends React.Component{
-  constructor( props ){
-    super( props );
+export default class NeedPanel extends React.Component {
+  constructor(props) {
+    super(props);
 
   }
 
-  render(){
-    const {value: {desc, location}} = this.props;
+  render() {
+    const {isLogin, value: {desc, location, type}} = this.props;
 
     return (
-        <div
-            className={"need-info"}
-        >
-          <div className="head">
-            <h2>{desc}</h2>
-          </div>
-          <div className="details">
-            <ul>
-              <li>{location}</li>
-            </ul>
+      <div
+        className={"need-info"}
+      >
+        <div className="head">
+          <h2>{desc}</h2>
+        </div>
+        <div className="details">
+          <ul>
+            <li><label>Location:</label></li>
+            <li><span>{location}</span></li>
+            <li><label>Type:</label></li>
+            <li><span>{type}</span></li>
+          </ul>
+          <div className="buttons">
+            {isLogin ?
+              <>
+                <Button variant="contained" onClick={this.props.onOffer}>Offer Help</Button>
+                <Button variant="contained" onClick={this.props.onCancel}>Close</Button>
+              </>
+              :
+              <Button className="close" variant="contained" onClick={this.props.onSignin}>Sign In</Button>
+            }
           </div>
         </div>
+      </div>
 
     );
   }
