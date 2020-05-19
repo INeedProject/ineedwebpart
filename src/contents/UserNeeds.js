651f46da34b73eb60723ff1e834ac7b3fbc1b26a
Rigormortis <myyilmaz97@gmail.com>
2020-05-19 14:05:54 +0300

    UserNeeds request event fix

--- src/contents/UserNeeds.js
+++ src/contents/UserNeeds.js
@@ -15,34 +15,23 @@ class UserNeeds extends Component {
         this.fetch=await this.props.rdb.ref('needs/');
         this.getUserNeeds();
     }
-    getUserNeeds =()=> {
-        let temp=[];
-        this.fetch.on('value', snapshot => {
-            snapshot.forEach(function(childsnaps){
-                let item=childsnaps.val();
-                item.key=childsnaps.key;
-                if(item.email===localStorage.getItem("email"))
-                    temp.push(item);
 
+    getUserNeeds = () => {
+        this.fetch.on('value', snapshot => {
+            let temp = [];
+            snapshot.forEach(function (childsnaps) {
+                let item = childsnaps.val();
+                item.key = childsnaps.key;
+                if (item.email === localStorage.getItem("email"))
+                    temp.push(item);
             });
+            this.setState({userneeds: temp});
         });
-        this.setState({userneeds:temp});
     };
-    showResults=(event)=>{
-        event.preventDefault();
-        this.setState({searched:true});
-        cogoToast.success("Results have found");
-    }
 
     render() {
         return (
             <div className="condiv">
-                <Button
-                    variant="contained"
-                    onClick={this.showResults}
-                >
-                    Get your posted needs
-                </Button>
                 <table className="container">
                     <thead>
                     <tr>
@@ -51,8 +40,8 @@ class UserNeeds extends Component {
                         <th><h1>Type</h1></th>
                     </tr>
                     </thead>
-                {this.state.searched===true?this.state.userneeds.map(((value, index) => <tbody><tr key={index}>
-                <td>{value.desc}</td><td>{value.location}</td><td>{value.type}</td></tr></tbody>)):<></>}
+                {this.state.userneeds.map(((value, index) => <tbody><tr key={index}>
+                <td>{value.desc}</td><td>{value.location}</td><td>{value.type}</td></tr></tbody>))}
                 </table>
             </div>
             )
